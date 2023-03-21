808dbaa7ae0494d117b6e932a23218f80c86fdc8
RichKing <RichKing@DESKTOP-RBL1U9L>
2023-03-21 07:54:40 -0700

    Add project files.

---
+++ Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Security.Principal;
+
+namespace MoneyBankAPI.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType  AccountType { get; set; } // This is an Enum will show if the account is a "savings" or a "current" account,
+        public string AccountNumberGenerated { get; set; }  //We shall generate the accountNumber here !
+
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+
+
+    public enum AccountType
+    {
+        Savings,
+        Current,
+        Corporate, 
+        Government
+    }
+}
